e0b35e490aa2ad48d675a9f72e3c955bcb29ed02
Ranganath Chenna <rchenna@akamai.com>
2019-05-26 17:31:08 +0530

    Intial Commit With TestNG+Selenium+Custom-Reporting
    Updated the ReadMe.md

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# anz-automation-task
-ANZ Automation Task
+**# ANZ Automation Task**
+
+**### Technologies - Used**
+
+- Java
+- TestNG
+- Selenium-Java
+- Lombok
+- AWS-Grid-lastic
+
+**###**  **Plugins**  **For Eclipse**
+
+- TestNG
+- Lombok
+  - https://projectlombok.org/setup/eclipse
+
+**### Commands To Run from terminal:**
+
+- **mvn**  **test -**** Dtest ****=BorrowingCalculatorTest-DrunLocal=true -DbrowserName=**** chrome**
+
+- [browserName] Values : chrome/firefox
+- [runLocal] : true/false
+  - if [true] :
+    - [chromeDriverInterfacePath] , [firefoxDriverInterfacePath] should be set based on the browser selection
+
+- [http://chromedriver.chromium.org/downloads](http://chromedriver.chromium.org/downloads)
+- [https://github.com/mozilla/geckodriver/releases](https://github.com/mozilla/geckodriver/releases)
+
+-
+  - if [false] :
+    -  set [gridHubHost] , [gridHubPort]
+      - Eg: -DgridHubHost=[IPAddress/HostName of Hub] -DgridHubPort=[Port of Hub]
+
+- test=BorrowingCalculatorTest
+
+            - This is to specify the TestClass which we want to run. If User want to tun specific method add [TestClass#[TestMethodName]]
+
+
+
+### **From Eclipse IDE:**
+
+- Install TestNG plugin
+  - Edit the automation.properties as per our requirement to run chrome/firefox -- LocalExecution / Grid Execution
+- Run As TestNG Class / TestNG Suite
--- src/main/java/com/automation/task/common/helpers/ConfigReader.java
+++ src/main/java/com/automation/task/common/helpers/ConfigReader.java
@@ -3,11 +3,14 @@ package com.automation.task.common.helpers;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Properties;
 
 import com.automation.task.common.CommonException;
 import com.automation.task.common.constants.ConfigData;
 import com.automation.task.common.constants.ConfigParams;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 /**
@@ -17,6 +20,7 @@ import com.automation.task.common.constants.ConfigParams;
  * @author Ranganath Chenna
  *
  */
+@Slf4j
 public class ConfigReader {
 
 	private static ConfigData configData;
@@ -55,9 +59,48 @@ public class ConfigReader {
 										firefoxDriverInterfacePath,
 										browser,
 										appHost);
+		
+		// Reading VmArgs and Update the Config Values
+		if( getSystemProperty(ConfigParams.BROWSER_NAME.getPropertyKey()).isPresent() ) {
+			configData.setBrowserName(getSystemProperty(ConfigParams.BROWSER_NAME.getPropertyKey()).get());
+		}
+		
+		if( getSystemProperty(ConfigParams.APP_HOST.getPropertyKey()).isPresent() ) {
+			configData.setAppHost(getSystemProperty(ConfigParams.APP_HOST.getPropertyKey()).get());
+		}
+		
+		if( getSystemProperty(ConfigParams.RUN_LOCAL.getPropertyKey()).isPresent() ) {
+			configData.setRunLocal(getSystemProperty(ConfigParams.RUN_LOCAL.getPropertyKey()).get());
+		}
+		
+		if( getSystemProperty(ConfigParams.GRID_HUB_HOST.getPropertyKey()).isPresent() ) {
+			configData.setGridHubHost(getSystemProperty(ConfigParams.GRID_HUB_HOST.getPropertyKey()).get());
+		}
+		
+		if( getSystemProperty(ConfigParams.GRID_HUB_PORT.getPropertyKey()).isPresent() ) {
+			String gridHubPortVmArg = getSystemProperty(ConfigParams.GRID_HUB_PORT.getPropertyKey()).get();
+			configData.setGridHubPort(Integer.parseInt(gridHubPortVmArg));
+		}
+		
+		if( getSystemProperty(ConfigParams.CHROME_DRIVER_EXE_PATH.getPropertyKey()).isPresent() ) {
+			configData.setChromeDriverInterfacePath(getSystemProperty(ConfigParams.CHROME_DRIVER_EXE_PATH.getPropertyKey()).get());
+		}
+		
+		if( getSystemProperty(ConfigParams.FIREFOX_DRIVER_EXE_PATH.getPropertyKey()).isPresent() ) {
+			configData.setFirefoxDriverInterfacePath(getSystemProperty(ConfigParams.FIREFOX_DRIVER_EXE_PATH.getPropertyKey()).get());
+		}
 	}
 
 	public static ConfigData getConfigData() {
 		return configData;
-	} 
+	}
+	
+	private static Optional<String> getSystemProperty(String key) {
+		logger.info("Fetching key {} from VM Args",key);
+		String value = System.getProperty(key);
+	    if (null == value || value.isEmpty()) {
+	        return Optional.empty();
+	    }
+	    return Optional.of(value);
+	}
 }
